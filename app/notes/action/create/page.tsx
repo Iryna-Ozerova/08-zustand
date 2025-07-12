63429cc889675dc130d4ead1dbc7846e922b752e
Irina Ozerova <marta.ilyashenko@gmail.com>
2025-07-12 14:21:54 +0300

    feat: move note creation to page and add draft autosave

---
+++ app/notes/action/create/page.tsx
@@ -0,0 +1,35 @@
+import NoteForm from "@/components/NoteForm/NoteForm";
+import css from "./CreateNote.module.css";
+
+export const metadata = {
+  title: "Create a new note",
+  description: "Start a fresh note to keep track of your ideas.",
+  openGraph: {
+    title: "Create a new note",
+    description: "Start a fresh note to keep track of your ideas.",
+    url: "https://notehub.com/notes/action/create",
+    siteName: "NoteHub",
+    images: [
+      {
+        url: "https://placehold.co/1200x630",
+        width: 1200,
+        height: 630,
+        alt: "Create note",
+      },
+    ],
+    type: "article",
+  },
+};
+
+export default function CreateNote() {
+  return (
+    <main className={css.main}>
+      <div className={css.container}>
+        <h1 className={css.title}>Create note</h1>
+        <NoteForm />
+      </div>
+    </main>
+  );
+}
+
+
